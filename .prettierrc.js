7879d550342805ae7c06bb8693d5827bb258e61c
alincode <alin.code@gmail.com>
2020-01-25 22:31:30 +0800

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// https://prettier.io/docs/en/options.html
+
+module.exports = {
+  trailingComma: 'es5',
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'always',
+}
